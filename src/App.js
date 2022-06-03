88aea7685f68a336b9d192d260b30e024b95aa12
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-06-03 16:54:20 +0200

    add functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-import "./App.scss"
+import "./App.scss";
+import { useState } from "react";
 function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ["/", "*", "+", "-", "."];
+
+  const updateCalc = (value) => {
+    if (
+      (ops.includes(value) && calc === "") ||
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    )
+      return;
+
+    setCalc(calc + value);
+
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  };
+
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+    setResult("");
+  };
+
+  const deleteLast = () => {
+    if (calc === "") {
+      return;
+    }
+
+    const value = calc.slice(0, -1);
+    setCalc(value);
+  };
+
   const createDigits = () => {
     const digits = [];
 
     for (let i = 1; i < 10; i++) {
-      digits.push(<button key={i}>{i}</button>);
+      digits.push(
+        <button key={i} onClick={() => updateCalc(i.toString())}>
+          {i}
+        </button>
+      );
     }
     return digits;
   };
@@ -13,20 +51,22 @@ function App() {
     <div className="App">
       <div className="calculator">
         <div className="display">
-          <span>(0)</span> 0
+          {result ? <span>({result})</span> : ""}
+          {calc || "0"}
         </div>
         <div className="operators">
-          <button>/</button>
-          <button>*</button>
-          <button>+</button>
-          <button>-</button>
+          <button onClick={() => updateCalc("/")}>/</button>
+          <button onClick={() => updateCalc("*")}>*</button>
+          <button onClick={() => updateCalc("+")}>+</button>
+          <button onClick={() => updateCalc("-")}>-</button>
+          <button onClick={deleteLast}>C</button>
         </div>
 
         <div className="digits">
           {createDigits()}
-          <button>0</button>
-          <button>.</button>
-          <button>=</button>
+          <button onClick={() => updateCalc("0")}>0</button>
+          <button onClick={() => updateCalc(".")}>.</button>
+          <button onClick={() => calculate()}>=</button>
         </div>
       </div>
     </div>
